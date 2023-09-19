cc23167cfa0d0555c49b32dd6c0ee7e097a328e6
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 14:55:05 -0400

    Add jwtUtils and authenticationManager attributes and loginUser method to UserService class.

--- src/main/java/com/example/miniproject/service/UserService.java
+++ src/main/java/com/example/miniproject/service/UserService.java
@@ -1,9 +1,12 @@
 package com.example.miniproject.service;
 
 import com.example.miniproject.model.User;
+import com.example.miniproject.model.request.LoginRequest;
 import com.example.miniproject.repository.UserRepository;
+import com.example.miniproject.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,13 +16,17 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
 
     @Autowired
     // @Lazy means to load it only when a user registers (only when needed)
-    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, JWTUtils jwtUtils, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtils - jwtUtils;
+        this.authenticationManager = authenticationManager;
     }
 
 
@@ -36,6 +43,23 @@ public class UserService {
 
     public User findUserByEmailAddress(String emailAddress) {
         return userRepository.findUserByEmailAddress(emailAddress);
+    }
+
+
+    public Optional<String> loginUser(LoginRequest loginRequest) {
+
+        UsernamePasswordAuthenticationToken authenticationToken = new
+                UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(), loginRequest.getPassword());
+
+
+        try {
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 
 }
