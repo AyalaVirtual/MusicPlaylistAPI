5aa52f9ddda80ac21090d9b8850d906c7906e343
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 14:49:25 -0400

    Create response package and LoginResponse class. Add jwt attribute, constructor, and getters and setters.

---
+++ src/main/java/com/example/miniproject/model/response/LoginResponse.java
@@ -0,0 +1,19 @@
+package com.example.miniproject.model.response;
+
+public class LoginResponse {
+
+    private String jwt;
+
+
+    public LoginResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+}
