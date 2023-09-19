2ec4dd5fa3bf7e68f70ccff903dbba6b6aef7c93
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 12:48:41 -0400

    Create dev profile and set port number.

--- src/main/java/com/example/miniproject/MiniprojectApplication.java
+++ src/main/java/com/example/miniproject/MiniprojectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniprojectApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MiniprojectApplication.class, args);
 	}
 
