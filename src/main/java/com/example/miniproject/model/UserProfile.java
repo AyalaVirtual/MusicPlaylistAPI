4320d4e966c847d6a2034ad57f8f6c43a3a71b15
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 12:59:36 -0400

    Add constructor and getters and setters.

--- src/main/java/com/example/miniproject/model/UserProfile.java
+++ src/main/java/com/example/miniproject/model/UserProfile.java
@@ -21,7 +21,46 @@ public class UserProfile {
     private String profileDescription;
 
 
+    public UserProfile(long id, String firstName, String lastName, String profileDescription, User user) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profileDescription = profileDescription;
+        this.user = user;
+    }
 
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
 
 
 
