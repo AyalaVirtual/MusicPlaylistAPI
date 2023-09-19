445e6c0d98a3dc31893fb15448eb77cd37da5e46
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 14:38:52 -0400

    Add authJwtRequestFilter and filterChain methods in SecurityConfiguration class

--- src/main/java/com/example/miniproject/security/SecurityConfiguration.java
+++ src/main/java/com/example/miniproject/security/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.miniproject.security;
 
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -15,7 +19,35 @@ public class SecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public JwtRequestFilter authJwtRequestFilter() {
+        return new JwtRequestFilter();
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // http represents traffic
+        http.authorizeRequests()
+                .antMatchers("/auth/users/", "/auth/users/login/", "/auth/users/register/")
+                .permitAll()
+                // .antMatchers() means allow matching patterns
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .and()
+                // CSRF stands for cross site request forgery
+                .csrf()
+                .disable()
+                .headers()
+                .frameOptions()
+                .disable();
+
+        http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 
 
 
