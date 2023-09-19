9ba78dac694755ee37212b5afe10836c83293065
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 14:45:35 -0400

    Create request package and LoginRequest class. Add emailAddress and password attributes and getters.

---
+++ src/main/java/com/example/miniproject/model/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.example.miniproject.model.request;
+
+
+public class LoginRequest {
+
+    private String emailAddress;
+    private String password;
+
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
