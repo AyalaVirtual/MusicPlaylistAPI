3bf9754f8b8156f4f40c7b0be64cfdfcc5db478f
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 13:06:04 -0400

    Create Genre class and add attributes, constructor, and getters and setters.

---
+++ src/main/java/com/example/miniproject/model/Genre.java
@@ -0,0 +1,62 @@
+package com.example.miniproject.model;
+
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+
+
+
+
+
+    public Genre(Long id, String name, String description, User user) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.user = user;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
+
+
+
+}
