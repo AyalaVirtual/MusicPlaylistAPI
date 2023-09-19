47b2e7f08e3761c86255c64888faf97962410be6
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 13:13:52 -0400

    Add getters and setters to Song class.

--- src/main/java/com/example/miniproject/model/Song.java
+++ src/main/java/com/example/miniproject/model/Song.java
@@ -33,7 +33,37 @@ public class Song {
     }
 
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
 
 
 
