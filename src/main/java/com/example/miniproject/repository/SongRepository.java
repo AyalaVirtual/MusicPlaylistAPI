2109e6cbb40a1da3cb73fcea93e6688560fe2a3d
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 21:25:24 -0400

    Correct errors in SongRepository. Add <Song, Long> to interface declaration and import Song class.

--- src/main/java/com/example/miniproject/repository/SongRepository.java
+++ src/main/java/com/example/miniproject/repository/SongRepository.java
@@ -1,9 +1,10 @@
 package com.example.miniproject.repository;
 
+import com.example.miniproject.model.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SongRepository extends JpaRepository {
+public interface SongRepository extends JpaRepository<Song, Long> {
 
 }
