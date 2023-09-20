35497c58c703d85db88b4498cb2e3fae6f7cf4be
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-19 21:00:33 -0400

    Add second GET method into GenreController to retrieve all genres.

--- src/main/java/com/example/miniproject/controller/GenreController.java
+++ src/main/java/com/example/miniproject/controller/GenreController.java
@@ -4,8 +4,12 @@ import com.example.miniproject.model.Genre;
 import com.example.miniproject.service.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -23,6 +27,11 @@ public class GenreController {
     @GetMapping(path = "/genres/") // http://localhost:9092/api/genres/
     public List<Genre> getGenres() {
         return genreService.getGenres();
+    }
+
+    @GetMapping(path = "/genres/{genreId}/") // http://localhost:9092/api/genres/1/
+    public Optional<Genre> getGenre(@PathVariable(value = "genreId") Long genreId) {
+        return genreService.getGenre(genreId);
     }
 
 
