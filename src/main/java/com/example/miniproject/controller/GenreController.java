cc073fa3b9b2928afe0238b8b3d0e2d4bdf53aeb
Erica Ayala <erica.ayala.work@gmail.com>
2023-09-20 08:13:24 -0400

    Add doc strings to GET methods in GenreController class.

--- src/main/java/com/example/miniproject/controller/GenreController.java
+++ src/main/java/com/example/miniproject/controller/GenreController.java
@@ -21,18 +21,33 @@ public class GenreController {
         this.genreService = genreService;
     }
 
-    // GET all genres
+    /**
+     * This sets the path for GET requests for all genres and links to the corresponding method in GenreService
+     *
+     * @return the result from calling the GET method for all genres in GenreService
+     */
     @GetMapping(path = "/genres/") // http://localhost:9092/api/genres/
     public List<Genre> getGenres() {
         return genreService.getGenres();
     }
 
-    // GET individual genre
+    /**
+     * This sets the path for GET requests for a specific genre and links to the corresponding method in GenreService
+     *
+     * @param genreId represents the specific genre by id
+     * @return the result from calling the GET method for a specific genre in GenreService
+     */
     @GetMapping(path = "/genres/{genreId}/") // http://localhost:9092/api/genres/1/
     public Optional<Genre> getGenre(@PathVariable(value = "genreId") Long genreId) {
         return genreService.getGenre(genreId);
     }
 
+    /**
+     *
+     *
+     * @param genreObject represents the genre the user is trying to create
+     * @return
+     */
     // POST genre
     @PostMapping(path = "/genres/") // http://localhost:9092/api/genres/
     public Genre createGenre(@RequestBody Genre genreObject) {
@@ -40,21 +55,25 @@ public class GenreController {
     }
 
 
-
-
-
-
-
-
-
-
+    /**
+     *
+     *
+     * @param genreId represents the specific genre by id
+     * @return
+     */
     // DELETE genre
     @DeleteMapping(path = "/genres/{genreId}") //  http://localhost:9092/api/genres/1/
     public Optional<Genre> deleteGenre(@PathVariable(value = "genreId") Long genreId) {
         return genreService.deleteGenre(genreId);
     }
 
-
+    /**
+     *
+     *
+     * @param genreId represents the specific genre by id
+     * @param songObject represents the song the user is trying to create
+     * @return
+     */
     // POST song to genre
     @PostMapping(path = "/genres/{genreId}/songs/") // http://localhost:9092/api/genres/1/songs/
     public Song createSong(@PathVariable(value = "genreId") Long genreId, @RequestBody Song songObject) {
@@ -62,22 +81,37 @@ public class GenreController {
         return genreService.createSong(genreId, songObject);
     }
 
+    /**
+     *
+     *
+     * @return
+     */
     // GET all songs
     @GetMapping(path = "/genres/songs/")
     public List<Song> getSongs(){
         return genreService.getSongs();
     }
 
+    /**
+     *
+     *
+     * @param genreId represents the specific genre by id
+     * @param songId represents the specific song by id
+     * @return
+     */
     // GET individual song
     @GetMapping(path = "/genres/{genreId}/songs/{songId}/")
     public Optional<Song> getSong(@PathVariable(value = "genreId") Long genreId, @PathVariable(value = "songId") Long songId){
         return genreService.getSong(genreId, songId);
     }
 
-
-
-
-
+    /**
+     *
+     *
+     * @param genreId represents the specific genre by id
+     * @param songId represents the specific song by id
+     * @return
+     */
     // DELETE song
     @DeleteMapping(path = "/genres/{genreId}/songs/{songId}/")
     public Optional<Song> deleteSong(@PathVariable(value = "genreId") Long genreId, @PathVariable(value = "songId") Long songId) {
